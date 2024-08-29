24af986a77bbe770f77c194567b8db27a61aeed3
Wairioko <charlesmungai5@gmail.com>
2024-08-29 20:49:21 +0300

    Configured multer to handle upload of images during blog creation

--- src/routes/blogRoutes.mjs
+++ src/routes/blogRoutes.mjs
@@ -1,6 +1,18 @@
 import express from 'express';
 import { getAllBlogs, getBlogById, createBlog, updateBlog, deleteBlog } from '../controllers/blogController.mjs';
 import { verifyToken } from '../auth/auth.mjs';
+import multer from 'multer';
+
+
+// Configure multer for handling file uploads
+const upload = multer({ 
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // limit file size to 5MB
+    },
+});
+
+
 
 
 const router = express.Router();
@@ -9,7 +21,7 @@ router.get('/api/blogs', getAllBlogs);
 
 router.get('/api/blog/:id', getBlogById);
 
-router.post('/api/create-blog', verifyToken, createBlog);
+router.post('/api/create-blog',  upload.array('images', 5) ,verifyToken, createBlog);
 
 router.put('/api/update-blog/:id',verifyToken,  updateBlog);
 
