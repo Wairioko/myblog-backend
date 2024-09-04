03f964cd489812a04232f981c1a0cf38be4783f1
Wairioko <charlesmungai5@gmail.com>
2024-09-04 14:03:50 +0300

    Added changes to include file url path for where the app is launched from

--- index.mjs
+++ index.mjs
@@ -5,6 +5,8 @@ import userRoutes from './src/routes/userRoutes.mjs';
 import rateLimit from 'express-rate-limit';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 dotenv.config();
 export const jwtSecret = process.env.jwtSecret;
@@ -20,14 +22,14 @@ mongoose.connect("mongodb://localhost:27017/my-blog")
 
 // Rate limiter
 const rateLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
+  windowMs: 60 * 60 * 1000, // 1 hour
   max: 100,
   message: "Too many requests made in the last hour"
 });
 
 // CORS options
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://myblog-frontend-5zy3.vercel.app/',
   optionsSuccessStatus: 200
 };
 
@@ -35,6 +37,8 @@ app.use((req, res, next) => {
   console.log(`${req.method} - ${req.url}`);
   next();
 });
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -45,6 +49,10 @@ app.use(rateLimiter);
 export default app; // Export the app for testing
 
 // If this file is run directly, start the server
-if (require.main === module) {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+if (process.argv[1] === __filename) {
   app.listen(port, () => console.log("Started server on port 4000"));
 }
+
