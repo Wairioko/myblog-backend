ab8e60e42d10388fe48f32f72724225fff988006
Wairioko <charlesmungai5@gmail.com>
2024-09-05 07:21:07 +0300

    vercel link updated

--- index.mjs
+++ index.mjs
@@ -33,7 +33,8 @@ const rateLimiter = rateLimit({
 
 // CORS options
 const corsOptions = {
-  origin: 'https://myblog-frontend-5zy3-5ygz0yiu2-wairioko-mungais-projects.vercel.app',
+  origin: 'https://myblog-frontend-5zy3-ni23779xf-wairioko-mungais-projects.vercel.app',
+  
   
   optionsSuccessStatus: 200
 };
